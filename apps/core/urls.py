ee6d8a78b3fe391e16858f4e2ff6a1b264b58b41
umaru <15875339926@139.com>
2024-12-17 18:06:43 +0800

    chore: enhance software features and update configurations
    - Change site title from "OpsPilot" to "Rewind Admin"
    
    Signed-off-by: umaru <15875339926@139.com>

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from apps.core.views import index_view
 from apps.core.views.user_view import UserView
 
-admin.site.site_title = "OpsPilot"
+admin.site.site_title = "Rewind Admin"
 admin.site.site_header = admin.site.site_title
 public_router = routers.DefaultRouter()
 public_router.register(r"api/public/user_view", UserView, basename="user_view")
