6a62c30fa11cd17618ed7dfe490bc090d1525421
Your Name <you@example.com>
2025-01-03 09:46:28 +0000

    for ops

--- support-files/docker/startup.sh
+++ support-files/docker/startup.sh
@@ -1,3 +1,4 @@
 python manage.py migrate
-python manage.py createsuperuser --noinput
-daphne -b 0.0.0.0 -p 8000 weops_lite.asgi:application
+python manage.py createcachetable django_cache
+python manage.py plugin_init
+supervisord -n
