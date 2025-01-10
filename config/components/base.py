e820ed80484d5036a524ff35efca4199d06498bb
zhmf7408 <kido@canway.net>
2025-01-10 17:12:38 +0800

    merge 合并修改

--- config/components/base.py
+++ config/components/base.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+if BASE_DIR.name == "config":
+    BASE_DIR = BASE_DIR.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 APP_CODE = os.getenv("APP_CODE", "rewind")
 
@@ -19,4 +20,3 @@ ASGI_APPLICATION = "asgi.application"
 DEBUG = os.getenv("DEBUG", "0") == "1"
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
-
