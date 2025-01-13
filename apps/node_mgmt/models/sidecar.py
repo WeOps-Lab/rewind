08bf05fb67c7fe045586393ff68835b3d45593cd
baiyufei <baiyfyx@163.com>
2025-01-13 15:22:28 +0800

    feature-配置文件id格式更改

--- apps/node_mgmt/models/sidecar.py
+++ apps/node_mgmt/models/sidecar.py
@@ -81,7 +81,7 @@ class CollectorConfiguration(TimeInfo, MaintainerInfo):
     # uuid
     def save(self, *args, **kwargs):
         if not self.id:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid.uuid4().hex)
         super().save(*args, **kwargs)
 
 
