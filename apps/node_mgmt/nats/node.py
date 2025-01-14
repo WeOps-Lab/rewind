8beee71400ab0a5a97f39e647aa2c2ee8ce58847
baiyufei <baiyfyx@163.com>
2025-01-14 14:53:06 +0800

    feature-更改节点管理rpc service的响应

--- apps/node_mgmt/nats/node.py
+++ apps/node_mgmt/nats/node.py
@@ -1,12 +1,10 @@
 import nats_client
 
 from apps.node_mgmt.child_config.common import ChildConfigCommon
-from apps.node_mgmt.nats import json_dumps_decorator
 from apps.node_mgmt.services.node import NodeService
 
 
 @nats_client.register
-@json_dumps_decorator
 def node_list(query_data: dict):
     """获取节点列表"""
     organization_ids = query_data.get('organization_ids')
@@ -19,13 +17,11 @@ def node_list(query_data: dict):
 
 
 @nats_client.register
-@json_dumps_decorator
 def collector_list(query_data: dict):
     return []
 
 
 @nats_client.register
-@json_dumps_decorator
 def batch_setting_node_child_config(data: dict):
     """批量对节点设置子配置"""
     object_type = data.get('object_type')
@@ -34,7 +30,6 @@ def batch_setting_node_child_config(data: dict):
 
 
 @nats_client.register
-@json_dumps_decorator
 def get_instance_child_config(query_data: dict):
     """获取实例子配置"""
     object_type = query_data.get('object_type')
