b352ced040a7e82223e3254f8827db6cc3549e06
zhmf7408 <kido@canway.net>
2025-01-14 15:53:02 +0800

    feature 修改返回格式

--- config/drf/serializers.py
+++ config/drf/serializers.py
@@ -2,8 +2,6 @@ from django.utils.translation import gettext as _
 from rest_framework import serializers
 from rest_framework.fields import empty
 
-from apps.core.utils.keycloak_client import KeyCloakClient
-
 
 class I18nSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
@@ -20,13 +18,7 @@ class TeamSerializer(I18nSerializer):
     def __init__(self, instance=None, data=empty, **kwargs):
         super().__init__(instance=instance, data=data, **kwargs)
         request = self.context["request"]
-        if not hasattr(request, "user"):
-            token = request.META.get("HTTP_AUTHORIZATION").split("Bearer ")[-1]
-            client = KeyCloakClient()
-            _, user_info = client.token_is_valid(token)
-            groups = client.get_user_groups(user_info["sub"], "admin" in user_info["realm_access"]["roles"])
-        else:
-            groups = request.user.group_list
+        groups = request.user.group_list
         self.group_map = {i["id"]: i["name"] for i in groups}
 
     def get_team_name(self, instance):
