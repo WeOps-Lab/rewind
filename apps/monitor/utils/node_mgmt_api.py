905816b913bb77c944310b4f43a77cb723acf663
baiyufei <baiyfyx@163.com>
2025-01-14 15:48:24 +0800

    feature-配置管理

--- apps/monitor/utils/node_mgmt_api.py
+++ apps/monitor/utils/node_mgmt_api.py
@@ -1,66 +1,20 @@
-import os
-
-import requests
+from apps.rpc.node_mgmt import NodeMgmt
 
 
-class NodeApi:
-    def __init__(self, token):
-        self.token = token
-        self.host = self.get_url()
+class NodeUtils:
 
-    def get_url(self):
-        url = os.getenv("NODE_MANAGE_URL")
-        if not url:
-            raise ValueError("NODE_MANAGE_URL is not set")
-        return url
+    @staticmethod
+    def get_nodes(query_data: dict):
+        return NodeMgmt().node_list(query_data)
 
-    def get_nodes(self, cloud_region_id, page, page_size, is_superuser, organization_ids: list):
-        url = f"{self.host}/node_mgmt/api/node"
-        headers = {"Authorization": f"Bearer {self.token}"}
-        params = {"cloud_region_id": cloud_region_id, "page": page, "page_size": page_size}
-        if not is_superuser:
-            params["organization_ids"] = ",".join(organization_ids)
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        return response.json()
+    @staticmethod
+    def batch_setting_node_child_config(data: dict):
+        return NodeMgmt().batch_setting_node_child_config(data)
 
-    def get_collectors(self, name=None, node_operating_system=None):
-        url = f"{self.host}/node_mgmt/api/collector"
-        params = {}
-        if name:
-            params["name"] = name
-        if node_operating_system:
-            params["node_operating_system"] = node_operating_system
-        headers = {"Authorization": f"Bearer {self.token}"}
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        return response.json()
+    @staticmethod
+    def get_instance_child_config(query_data: dict):
+        return NodeMgmt().get_instance_child_config(query_data)
 
-    def get_config_detail(self, config_id):
-        url = f"{self.host}/node_mgmt/api/configuration/{config_id}"
-        headers = {"Authorization": f"Bearer {self.token}"}
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        return response.json()
-
-    def create_config(self, data: dict):
-        url = f"{self.host}/node_mgmt/api/configuration"
-        headers = {"Authorization": f"Bearer {self.token}"}
-        response = requests.post(url, headers=headers, json=data)
-        response.raise_for_status()
-        return response.json()
-
-    def update_config(self, id: str, data: dict):
-        url = f"{self.host}/node_mgmt/api/configuration/{id}"
-        headers = {"Authorization": f"Bearer {self.token}"}
-        response = requests.patch(url, headers=headers, json=data)
-        response.raise_for_status()
-        return response.json()
-
-    def node_asso_config(self, node_id, config_id):
-        url = f"{self.host}/node_mgmt/api/configuration/apply_to_node"
-        headers = {"Authorization": f"Bearer {self.token}"}
-        data = {"node_id": node_id, "collector_configuration_id": config_id}
-        response = requests.post(url, headers=headers, json=data)
-        response.raise_for_status()
-        return response.json()
+    @staticmethod
+    def update_instance_child_config(data: dict):
+        return NodeMgmt().update_instance_child_config(data)
--- apps/monitor/views/node_mgmt.py
+++ apps/monitor/views/node_mgmt.py
@@ -4,91 +4,99 @@ from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 
 from apps.core.utils.web_utils import WebUtils
-from apps.monitor.utils.node_mgmt_api import NodeApi
+from apps.monitor.utils.node_mgmt_api import NodeUtils
 
 
 class NodeMgmtView(ViewSet):
     @swagger_auto_schema(
         operation_description="查询节点列表",
-        manual_parameters=[
-            openapi.Parameter("cloud_region_id", openapi.IN_QUERY, description="云区域ID", type=openapi.TYPE_INTEGER,
-                              required=False),
-            openapi.Parameter("page", openapi.IN_QUERY, description="页码", type=openapi.TYPE_INTEGER, required=False),
-            openapi.Parameter("page_size", openapi.IN_QUERY, description="每页数据条数", type=openapi.TYPE_INTEGER,
-                              required=False),
-        ],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "cloud_region_id": openapi.Schema(type=openapi.TYPE_INTEGER, description="云区域ID"),
+                "page": openapi.Schema(type=openapi.TYPE_INTEGER, description="页码"),
+                "page_size": openapi.Schema(type=openapi.TYPE_INTEGER, description="每页数据条数"),
+            },
+            required=["cloud_region_id", "page", "page_size"]
+        ),
         tags=['NodeMgmt']
     )
-    @action(methods=['get'], detail=False, url_path='nodes')
+    @action(methods=['post'], detail=False, url_path='nodes')
     def get_nodes(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        cloud_region_id = request.GET.get("cloud_region_id", 1)
-        page = request.GET.get("page", 1)
-        page_size = request.GET.get("page_size", 999)
-        data = NodeApi(token).get_nodes(cloud_region_id, page, page_size, request.user.is_superuser,
-                                        request.user.group_list)
+        organization_ids = [] if request.user.is_superuser else [i["id"] for i in request.user.group_list]
+        data = NodeUtils.get_nodes(dict(
+            cloud_region_id=request.data.get("cloud_region_id", 1),
+            organization_ids=organization_ids,
+            name=request.data.get("name"),
+            ip=request.data.get("ip"),
+            os=request.data.get("os"),
+            page=request.data.get("page", 1),
+            page_size=request.data.get("page_size", 10),
+        ))
         return WebUtils.response_success(data)
 
     @swagger_auto_schema(
-        operation_description="查询采集器列表",
-        tags=['NodeMgmt']
-    )
-    @action(methods=['get'], detail=False, url_path='collectors')
-    def get_collectors(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        data = NodeApi(token).get_collectors()
-        return WebUtils.response_success(data)
-
-    @swagger_auto_schema(
-        operation_description="查询采集器配置详情",
-        manual_parameters=[
-            openapi.Parameter("collector_id", openapi.IN_PATH, description="采集器id", type=openapi.TYPE_INTEGER,
-                              required=True),
-        ],
-        tags=['NodeMgmt']
-    )
-    @action(methods=['get'], detail=False, url_path='config_detail/(?P<config_id>[^/.]+)')
-    def config_detail(self, request, config_id):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        data = NodeApi(token).get_config_detail(config_id)
-        return WebUtils.response_success(data)
-
-    @swagger_auto_schema(
-        operation_description="创建采集器配置",
+        operation_description="批量设置节点子配置",
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                "cloud_region_id": openapi.Schema(type=openapi.TYPE_INTEGER, description="配置信息"),
-                "name": openapi.Schema(type=openapi.TYPE_STRING, description="采集器ID"),
-                "collector_id": openapi.Schema(type=openapi.TYPE_STRING, description="配置信息"),
-                "config_template": openapi.Schema(type=openapi.TYPE_STRING, description="配置信息"),
-                "node_id": openapi.Schema(type=openapi.TYPE_STRING, description="节点id"),
+                "object_type": openapi.Schema(type=openapi.TYPE_STRING, description="对象类型"),
+                "nodes": openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            "id": openapi.Schema(type=openapi.TYPE_STRING, description="节点ID"),
+                            "configs": openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Schema(type=openapi.TYPE_OBJECT))
+                        }
+                    )
+                )
             },
-            required=["cloud_region_id", "name", "collector_id", "config_template", "node_id"]
+            required=["object_type", "nodes"]
         ),
-        tags=['NodeMgmt']
     )
-    @action(methods=['post'], detail=False, url_path='create_config')
-    def create_collector_config(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        data = NodeApi(token).create_config(request.data)
-        NodeApi(token).node_asso_config(request.data.get("node_id"), data.get("id"))
+    @action(methods=['post'], detail=False, url_path='batch_setting_node_child_config')
+    def batch_setting_node_child_config(self, request):
+        data = NodeUtils.batch_setting_node_child_config(request.data)
         return WebUtils.response_success(data)
 
     @swagger_auto_schema(
-        operation_description="更新采集器配置",
+        operation_description="查询实例子配置",
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                "name": openapi.Schema(type=openapi.TYPE_STRING, description="采集器ID"),
-                "config_template": openapi.Schema(type=openapi.TYPE_STRING, description="配置信息"),
+                "object_type": openapi.Schema(type=openapi.TYPE_STRING, description="对象类型"),
+                "collect_type": openapi.Schema(type=openapi.TYPE_STRING, description="采集类型"),
+                "config_type": openapi.Schema(type=openapi.TYPE_STRING, description="配置类型"),
+                "collect_instance_id": openapi.Schema(type=openapi.TYPE_STRING, description="采集实例ID"),
             },
-            required=["name", "config_template"]
+            required=["object_type", "collect_instance_id"]
         ),
         tags=['NodeMgmt']
     )
-    @action(methods=['post'], detail=False, url_path='update_config/(?P<config_id>[^/.]+)')
-    def update_config(self, request, config_id):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        data = NodeApi(token).update_config(config_id, request.data)
+    @action(methods=['post'], detail=False, url_path='get_instance_child_config')
+    def get_instance_child_config(self, request):
+        data = NodeUtils.get_instance_child_config(dict(
+            object_type=request.data.get("object_type"),
+            collect_type=request.data.get("collect_type"),
+            config_type=request.data.get("config_type"),
+            collect_instance_id=request.data.get("collect_instance_id"),
+        ))
+        return WebUtils.response_success(data)
+
+    @swagger_auto_schema(
+        operation_description="更改子配置",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "id": openapi.Schema(type=openapi.TYPE_STRING, description="子配置id"),
+                "content": openapi.Schema(type=openapi.TYPE_STRING, description="子配置内容"),
+            },
+            required=["id", "content"]
+        ),
+        tags=['NodeMgmt']
+    )
+    @action(methods=['post'], detail=False, url_path='update_instance_child_config')
+    def update_instance_child_config(self, request):
+        data = NodeUtils.update_instance_child_config(request.data)
         return WebUtils.response_success(data)
