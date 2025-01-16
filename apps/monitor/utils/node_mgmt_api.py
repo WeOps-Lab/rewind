1628b668f0d189bef145998ceb59d5f8e372d188
baiyufei <baiyfyx@163.com>
2025-01-16 11:35:09 +0800

    feature-监控接入

--- apps/monitor/utils/node_mgmt_api.py
+++ apps/monitor/utils/node_mgmt_api.py
@@ -1,4 +1,98 @@
+from apps.monitor.models import MonitorInstance
 from apps.rpc.node_mgmt import NodeMgmt
+
+
+class FormatChildConfig:
+
+    @staticmethod
+    def collector(data):
+        object_type = data["collect_type"]
+        configs = data["configs"]
+        instances = data["instances"]
+        if object_type == "host":
+            return FormatChildConfig.format_host(configs, instances)
+        elif object_type == "ping":
+            return FormatChildConfig.format_ping(instances, configs)
+
+
+    @staticmethod
+    def format_host(configs, instances):
+        result = {"object_type": "host", "nodes": []}
+
+        for instance in instances:
+            instance_id, instance_type = instance["instance_id"], instance["instance_type"]
+
+            for node_id in instance["nodes"]:
+                node_info = {"id": node_id, "configs": []}
+
+                for config in configs:
+                    node_info["configs"].append({
+                        "type": config["type"],
+                        "instance_id": instance_id,
+                        "instance_type": instance_type,
+                    })
+
+                result["nodes"].append(node_info)
+
+        return result
+
+    @staticmethod
+    def format_ping(instances, configs):
+        result = {"object_type": "ping", "nodes": []}
+
+        for instance in instances:
+            instance_id, instance_type = instance["instance_id"], instance["instance_type"]
+            url = instance["url"]
+
+            for node_id in instance["nodes"]:
+                node_info = {"id": node_id, "configs": []}
+
+                for config in configs:
+                    node_info["configs"].append({
+                        "type": config["type"],
+                        "instance_id": instance_id,
+                        "instance_type": instance_type,
+                        "url": url,
+                    })
+
+                result["nodes"].append(node_info)
+
+        return result
+
+    @staticmethod
+    def format_web(instances, configs):
+        pass
+
+    @staticmethod
+    def format_trap(instances, configs):
+        pass
+
+    @staticmethod
+    def format_ipmi(instances, configs):
+        result = {"collect_type": "ipmi", "nodes": []}
+
+        for instance in instances:
+            instance_id, instance_type = instance["instance_id"], instance["instance_type"]
+            ip = instance["ip"]
+
+            for node_id in instance["nodes"]:
+                node_info = {"id": node_id, "configs": []}
+
+                for config in configs:
+                    node_info["configs"].append({
+                        "type": config["type"],
+                        "instance_id": instance_id,
+                        "instance_type": instance_type,
+                        "server": f"{config['username']}:{config['password']}@{config['protocol']}({ip})",
+                    })
+
+                result["nodes"].append(node_info)
+
+        return result
+
+    @staticmethod
+    def format_snmp(instances, configs):
+        pass
 
 
 class NodeUtils:
