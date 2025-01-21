27f3317d0003eae9baa1a415c4365b684f2d825b
baiyufei <baiyfyx@163.com>
2025-01-21 14:23:45 +0800

    feature-调整实例配置查询逻辑

--- apps/monitor/services/node_mgmt.py
+++ apps/monitor/services/node_mgmt.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from apps.monitor.utils.node_mgmt_api import NodeUtils
 from apps.monitor.utils.victoriametrics_api import VictoriaMetricsAPI
 
@@ -16,7 +18,7 @@ class InstanceConfigService:
             config_map = {(i["collect_instance_id"], i["collect_type"]): i for i in configs}
 
         metrics = VictoriaMetricsAPI().query(pmq, "1h")
-        instance_configs = []
+        instance_config_map = {}
         for metric_info in metrics.get("data", {}).get("result", []):
             instance_id = metric_info.get("metric", {}).get("instance_id")
             if not instance_id:
@@ -38,6 +40,44 @@ class InstanceConfigService:
                 config_id=other_config_info.get("id"),
                 content=other_config_info.get("content"),
             )
-            instance_configs.append(config_info)
+            instance_config_map[key] = config_info
 
-        return instance_configs
+        # 补充未查询到的配置，但是在数据库中存在的配置
+        for key, config_info in config_map.items():
+
+            if key not in instance_config_map:
+                instance_config_map[key] = {
+                    "instance_id": config_info["collect_instance_id"],
+                    "collect_type": config_info["collect_type"],
+                    "config_type": config_info["config_type"],
+                    "agent_id": config_info.get("agents")[0],
+                    "time": 0,
+                    "config_id": config_info["id"],
+                    "content": config_info["content"],
+                }
+
+        # 状态计算
+        result = []
+        for conf_info in instance_config_map.values():
+            if conf_info["time"] == 0:
+                conf_info["status"] = ""
+            else:
+                conf_info["status"] = InstanceConfigService.calculation_status(conf_info["time"])
+            result.append(conf_info)
+
+        return result
+
+    @staticmethod
+    def calculation_status(data_time: int):
+        """计算状态"""
+        # 获取当前时间时间戳，utc0时区的
+        now_timestamp = int(datetime.now(timezone.utc).timestamp())
+        # 计算时间差
+        time_diff = now_timestamp - data_time
+        # 5分钟内正常，1小时内不活跃，1小时以上异常
+        if time_diff < 300:
+            return "normal"
+        elif time_diff < 3600:
+            return "inactive"
+        else:
+            return "unavailable"
