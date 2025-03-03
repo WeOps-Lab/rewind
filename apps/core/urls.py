f21c065a80365e1f7b71256b2d79c28aa9b0abd0
zhmf7408 <kido@canway.net>
2025-03-03 16:34:11 +0800

    feature 新增接口

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 
 from apps.core.views import index_view
 
-admin.site.site_title = "Rewind Admin"
+admin.site.site_title = "Opspilot Admin"
 admin.site.site_header = admin.site.site_title
 public_router = routers.DefaultRouter()
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     re_path(r"api/get_my_client/", index_view.get_my_client),
     re_path(r"api/get_client_detail/", index_view.get_client_detail),
     re_path(r"api/get_user_menus/", index_view.get_user_menus),
+    re_path(r"api/get_all_groups/", index_view.get_all_groups),
     path("select2/", include("django_select2.urls")),
 ]
 
