42e81f4fea44e49ed1d793f651a02ed0b68e8abc
zhmf7408 <kido@canway.net>
2025-03-03 17:29:52 +0800

    feature 菜单优化

--- apps/channel_mgmt/serializers/channel_serializer.py
+++ apps/channel_mgmt/serializers/channel_serializer.py
@@ -7,6 +7,27 @@ class ChannelSerializer(I18nSerializer):
         model = Channel
         fields = "__all__"
 
+    def create(self, validated_data):
+        if validated_data["channel_type"] == "email":
+            validated_data["config"] = {
+                "smtp_server": "",
+                "port": 0,
+                "smtp_user": "",
+                "smtp_pwd": "",
+                "smtp_usessl": False,
+                "smtp_usetls": False,
+                "mail_sender": "",
+            }
+        else:
+            validated_data["config"] = {
+                "corp_id": "",
+                "secret": "",
+                "token": "",
+                "aes_key": "",
+                "agent_id": "",
+            }
+        return super().create(validated_data)
+
 
 class ChannelTemplateSerializer(I18nSerializer):
     class Meta:
