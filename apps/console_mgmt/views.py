d9c789ff542f155e776b005b9e698e6b3c5db8c1
zhmf7408 <kido@canway.net>
2025-03-14 14:58:36 +0800

    feature 新增接口

--- apps/console_mgmt/views.py
+++ apps/console_mgmt/views.py
@@ -13,9 +13,9 @@ def get_opspilot_quota(request):
 def init_user_set(request):
     kwargs = json.loads(request.body)
     client = SystemMgmt()
-    res = client.create_default_group(kwargs["group_name"], kwargs["user_id"])
+    res = client.create_default_group(kwargs["group_name"], kwargs["user_id"], request.user.group_list[0]["id"])
     if not res["result"]:
         return JsonResponse(res)
     opspilot_client = OpsPilot()
-    res = opspilot_client.init_user_set(request.user.username, res["data"]["group_id"])
+    res = opspilot_client.init_user_set(res["data"]["group_id"], kwargs["group_name"])
     return JsonResponse(res)
