6d4c5c8cc6b39375080f0c379843668f37f9eac6
zhmf7408 <kido@canway.net>
2025-03-17 10:49:58 +0800

    feature 初始化删除

--- apps/channel_mgmt/services/channel_init_service.py
+++ apps/channel_mgmt/services/channel_init_service.py
@@ -1,10 +1,7 @@
-from apps.core.logger import logger
-
-
 class ChannelInitService:
     def __init__(self, owner):
         self.owner = owner.username
 
     @staticmethod
     def init():
-        logger.info("初始化监控告警模板")
+        pass
