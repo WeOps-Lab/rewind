5667c49372e9ca17b548dded820f984385daf64e
baiyufei <baiyfyx@163.com>
2025-03-21 14:45:49 +0800

    feature-告警列表添加实例查询

--- apps/monitor/filters/monitor_alert.py
+++ apps/monitor/filters/monitor_alert.py
@@ -15,6 +15,7 @@ class CommaSeparatedCharInFilter(BaseInFilter, CharFilter):
 
 
 class MonitorAlertFilter(FilterSet):
+    monitor_instance_id = CharFilter(field_name="monitor_instance_id", lookup_expr="exact", label="监控实例ID")
     status_in = CommaSeparatedCharInFilter(field_name="status", lookup_expr="in", label="状态")
     level_in = CommaSeparatedCharInFilter(field_name="level", lookup_expr="in", label="告警级别")
     created_at = IsoDateTimeFromToRangeFilter(field_name="created_at", label="创建时间范围")
@@ -22,4 +23,4 @@ class MonitorAlertFilter(FilterSet):
 
     class Meta:
         model = MonitorAlert
-        fields = ["status_in", "level_in", "created_at"]
+        fields = ["monitor_instance_id", "status_in", "level_in", "created_at", "content"]
